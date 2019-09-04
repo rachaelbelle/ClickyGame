d5152aec34ec38590eb5731dbd7f8c1c8b44f02d
YvelStLaurent <rachaelbelle@gmail.com>
2019-09-04 18:02:12 -0400

    fixing top centering

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = props => {
       Score {props.score}  Message {props.message} Top Score {props.topScore}
       </div> */}
       <div className="score col-md-3">Score {props.score}</div>
-      <div className="score col-md-6">Message {props.message}</div>
+      <div className="score col-md-5">Message {props.message}</div>
       <div className="score col-md-3">Top Score {props.topScore}</div>
     </div>
   );
